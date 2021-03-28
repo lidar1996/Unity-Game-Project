3594fa5f8d49a6509e82cd57f8833ce7f5201ab7
lidar1996 <lidar602@gmail.com>
2021-03-28 13:25:35 +0300

    changes

---
+++ Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource doorSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        doorSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("opening", true);
+        doorSound.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("opening", false);
+        doorSound.Play();
+    }
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/FPMotion.cs
+++ Assets/Scripts/FPMotion.cs
@@ -4,38 +4,45 @@ using UnityEngine;
 
 public class FPMotion : MonoBehaviour
 {
+    public GameObject playerCamera; // needs to be connected to real object in the ide 
     private CharacterController controller;
-    private float speed = 4f;
+    private float speed = 6f;
     private float rx = 0, ry;
     private float angularSpeed = 5;
+    private AudioSource stepFootSound;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();//gets player controller
-
+        stepFootSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //mouse
         rx -= Input.GetAxis("Mouse Y") * angularSpeed;
-        //ry += Input.GetAxis("Mouse X") * angularSpeed;
+        //use Clampf to limit sight angles
         ry = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * angularSpeed;
 
+        playerCamera.transform.localEulerAngles = new Vector3(rx, 0, 0);
 
-        transform.localEulerAngles = new Vector3(rx, ry, 0);//sets new orientation of player
-
-
-
+        transform.localEulerAngles = new Vector3(0, ry, 0);//sets new orientation of player
 
         //keyboard
         float dx, dz;
         dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;//horizontal means "A" "D"        Vector3 motion = new Vector3(dx, 0, dz);
         dz = Input.GetAxis("Vertical") * speed * Time.deltaTime;//W or S
+
         Vector3 motion = new Vector3(dx, 0, dz);
+
         motion = transform.TransformDirection(motion);
         controller.Move(motion);
 
+
+        //add sound of foot steps
+        if (!stepFootSound.isPlaying && controller.velocity.magnitude > 0.1)
+            stepFootSound.Play();
     }
 }
