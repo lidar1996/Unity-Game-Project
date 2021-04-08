d3d402b02b439581c8ab95014e5daaf901fc6a08
lidar1996 <lidar602@gmail.com>
2021-04-08 11:55:08 +0300

    Sliding doors added

---
+++ Assets/Scripts/SlidingDoorsMotion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoorsMotion : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        sound = GetComponent<AudioSource>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("doorOpen", true);
+        sound.Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("doorOpen", false);
+        sound.PlayDelayed(0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
